61b8c7a2f4ba250b7585f672ca1474ff4f510bd5
eddymarg <edgregana@gmail.com>
2025-02-24 20:31:18 -0500

    Updated with grid layout

--- frontend/my-react-app/src/pages/loggedinHome.jsx
+++ frontend/my-react-app/src/pages/loggedinHome.jsx
@@ -1,12 +1,28 @@
-import { Typography } from "@mui/material"
+"use client"
+import { Box } from "@mui/material"
+import { useState } from "react"
+import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps"
+import Grid from '@mui/material/Grid'
 import LoggedInHeader from "../components/loggedinHeader"
 
 const userHome = () => {
+    const position = { lat: 53.54, lng: 10 }
     return(
-        <div>
-            <LoggedInHeader/>
-            <Typography>Logged in!</Typography>
-        </div>
+        <APIProvider apiKey={import.meta.GOOGLE_MAP_API_KEY}>
+            <div>
+                <LoggedInHeader/>
+                <Grid container sx={{ height: "100vh"}}>
+                    <Grid item xs={12} md={6}>
+                        
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Box sx={{ height: '100%'}}>
+                            <Map zoom={9} center={position}></Map>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </div>
+        </APIProvider>
     )
 }
 
