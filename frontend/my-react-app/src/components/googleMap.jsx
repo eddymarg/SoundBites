8cdd397a2de2ad9f0a06f09261772257ae086b47
eddymarg <edgregana@gmail.com>
2025-03-24 21:34:42 -0400

    Updated modal
    - I added the google map directions

--- frontend/my-react-app/src/components/googleMap.jsx
+++ frontend/my-react-app/src/components/googleMap.jsx
@@ -2,16 +2,30 @@
 import { useState, useEffect } from "react"
 import { Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps"
 import '../css/googleModal.css'
-import { Box, Typography, Rating, Divider } from "@mui/material"
+import { Box, Typography, Rating, Divider, Stack } from "@mui/material"
 import LocalOfferIcon from '@mui/icons-material/LocalOffer';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import LanguageIcon from '@mui/icons-material/Language';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import CloseIcon from '@mui/icons-material/Close';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 
 const GoogleMap = ({ userLocation, restaurants, error, isLoading, selectedLocation, setSelectedLocation }) => {
     const fallbackLocation = { lat: 40.7128, lng: -74.0060 }
+
+    const showInMapClicked = () => {
+        if (selectedLocation?.place_id) {
+            window.open(`https://www.google.com/maps/search/?api=1&query=place_id:${selectedLocation.place_id}`, "_blank");
+        } else if (selectedLocation?.geometry?.location) {
+            const { lat, lng } = selectedLocation.geometry.location;
+            window.open(`https://www.google.com/maps?q=${lat},${lng}`, "_blank");
+        } else {
+            console.error("Location data is missing");
+        }
+    };
+    
+    
 
     return (
         <>
@@ -54,7 +68,10 @@ const GoogleMap = ({ userLocation, restaurants, error, isLoading, selectedLocati
                                 <img src={selectedLocation.photo} alt={selectedLocation.name} />
                                 <button onClick={() => setSelectedLocation(null)} style={{color: "white"}}><CloseIcon/></button>
                             </Box>
-                            <Typography variant="h4" style={{fontFamily: "'Tinos', serif", fontWeight: "700"}}>{selectedLocation.name}</Typography>
+                            <Stack direction="row" sx={{ justifyContent: "space-between", alignItems: "center"}}>
+                                <Typography variant="h4" style={{fontFamily: "'Tinos', serif", fontWeight: "700"}}>{selectedLocation.name}</Typography>
+                                <Typography><LocationOnIcon fontSize="large" onClick={showInMapClicked}/></Typography>
+                            </Stack>
                             <Box display="flex" alignItems="center" gap={1} sx={{marginBottom: '0.5rem'}}>
                                 <Typography>
                                     {selectedLocation.rating ? selectedLocation.rating.toFixed(1) : "N/A"}
